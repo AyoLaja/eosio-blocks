80e3080fe3812255f9a5d1462d67037f8a071c1d
Ayomide Onalaja <ayo.onalaja@gmail.com>
2019-10-09 14:34:41 -0400

    added transition

--- src/App.vue
+++ src/App.vue
@@ -45,6 +45,18 @@ export default {
     /* height: 100vh; */
   }
 
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+
   @media only screen and (min-width: 1317px) {
     #app {
       width: 1128px;
